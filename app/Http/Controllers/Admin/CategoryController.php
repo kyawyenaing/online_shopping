2ea1ce8f7059f3c8a83244f000bdeca5ef63acc1
Tun Lin Aung <htunlinaung153251@gmail.com>
2020-08-08 21:39:01 +0630

    web.php,homecontroller,app.blade,home.blade

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //
+    // public function __construct() {
+    //     $this->middleware('auth');
+    // }
+    public function index(){
+        dd('hello');
+    }
+}
